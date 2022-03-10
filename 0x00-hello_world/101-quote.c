31f3e2fa4fdd431e6782e9f7d4348dd95adf2753
Tim254 <tim.mailu@gmail.com>
2022-03-10 22:19:49 +0300

    standard erro

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - and that piece of art is useful
- * Return: 0 if exited properly, non zero otherwise
+ * main - Entry point
+ * Return:1
  */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful"
-- Dora Korpar, 2015 - 10 - 19\n");
-return (0);
+char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, ch, sizeof(ch));
+return (1);
 }
