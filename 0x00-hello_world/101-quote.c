cd61ff5aa070961887ca7aeffaa9aabecb801370
Tim254 <tim.mailu@gmail.com>
2022-03-10 22:18:16 +0300

    std error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - and that piece of art is useful
+ * Return: 0 if exited properly, non zero otherwise
+ */
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful"
+- Dora Korpar, 2015 - 10 - 19\n");
+return (0);
+}
