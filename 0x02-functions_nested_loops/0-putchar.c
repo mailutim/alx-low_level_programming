d7ef273bb1c978b53c36191e73813283412b7c4e
Tim254 <tim.mailu@gmail.com>
2022-03-15 21:45:43 +0300

    functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - program that prints _putchar.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int n = 0;
+char _putchar[8] = "_putchar";
+while (n < 8)
+{
+putchar(_putchar[n]);
+n += 1;
+}
+putchar("\n");
+
+return (0);
+}
